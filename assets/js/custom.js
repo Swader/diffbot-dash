24e1a4c7ca08134cab9546a3045474a8eba3ea10
Bruno Skvorc <bruno@skvorc.me>
2015-11-29 15:56:04 +0100

    Added vue HTTP and test API call, added save-on-enter, extracted save/load functions

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,21 +5,64 @@ var ti = document.getElementById('tokenInput');
 var vm = new Vue({
     el: '#app',
     data: {
-        token: localStorage.getItem('token') || ''
+        token: loadData('token')
     },
     methods: {
         tokensave: function() {
             this.token = ti.value;
-            localStorage.setItem('token', this.token);
+            saveData({token: this.token});
 
         },
         tokenshow: function() {
-            console.log("here1");
             if (ti.type == "password") {
                 ti.type = "text";
             } else {
                 ti.type = "password";
             }
+        },
+        doThis: function() {
+            this.$http.get('/api.php', {}, function(data, status, request) {
+                console.log(data, status, request);
+            });
         }
+    },
+    http: {
+        root: '/'
     }
 });
+
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    Non-vue logic here
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ **/
+
+/**
+ * Saves data to localStorage, clears old data first
+ * @param data
+ */
+function saveData(data) {
+    if (data !== undefined) {
+        saveData();
+
+        for (property in Object.keys(data))
+
+        for (var property in data) {
+            if (data.hasOwnProperty(property)) {
+                localStorage.setItem(property, data[property]);
+            }
+        }
+    } else {
+        localStorage.clear();
+    }
+}
+
+/**
+ * Loads given value from localStorage, or empty string if it isn't set.
+ * @param key
+ * @returns {string}
+ */
+function loadData(key) {
+    if (key !== undefined) {
+        return localStorage.getItem(key) || '';
+    }
+    console.error("Key must be defined! I don't know what to load!");
+}
---
+++ public/api.php
@@ -0,0 +1,3 @@
+<?php
+
+die(json_encode(["message" => "hello world"]));
