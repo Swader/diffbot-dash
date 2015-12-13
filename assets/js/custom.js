d59fc22996ade9cd2e38996d40ca303a0c66aa49
Bruno Skvorc <bruno@skvorc.me>
2015-12-13 22:59:59 +0100

    Added invoices and chart caching, rewrote JS slightly

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,41 +1,42 @@
-/** Put your custom JS code here **/
-
-// This input element will be reused a couple of times, so it's best to cache it here
 // @todo: convert it into a VueJS component?
 var ti = document.getElementById('tokenInput');
 
-// Format for Moment.js
-var format = "DD.MM.YYYY.";
-
-// Basic skeleton of the initial data strucutre of the Vue app.
-// Still debating with self if this is the way to go.
-var info = {
-    "name": null,
-    "plan": null,
-    "token": null,
-    "days": 31,
-    "callsChart": {
-        "startDate": moment().subtract(31, 'days'),
-        "endDate": moment()
-    }
+var defaultDays = 31;
+var defaultInfo = {
+    name: null,
+    plan: null,
+    token: null,
+    range: null,
+    calls: null,
+    invoices: null
 };
 
 // When set to true, outputs full stack trace with every warning
 Vue.config.debug = true;
 
+Vue.filter('momentify', function (value) {
+    return moment(value).format("DD.MM.YYYY");
+});
+
 var vm = new Vue({
     el: '#app',
     data: {
-        info: info,
-        // Initialize basic UI flags here - overlay is used when loading
-        ui: {
-            overlay: false
+        info: defaultInfo,
+        options: {
+            overlay: false,
+            dateFormat: "DD.MM.YYYY.",
+            apiUrl: "/api.php",
+            callsChart: {
+                "days": defaultDays,
+                "startDate": moment().subtract(defaultDays, 'days'),
+                "endDate": moment()
+            }
         }
     },
     ready: function () {
-        this.info = jQuery.extend(true, this.info, this.loadData());
+        this._loadData();
         if (this.info.token) {
-            this.requestData(this.info.token, this.info.days);
+            this._requestData(this.info.token, this.options.callsChart.days);
         }
 
         this.$nextTick(function () {
@@ -43,9 +44,10 @@ var vm = new Vue({
         });
     },
     methods: {
-        tokensave: function () {
-            this.saveData();
-            this.requestData(ti.value);
+        tokensave: function (event) {
+            event.target.blur();
+            localStorage.clear();
+            this._requestData(ti.value);
             this.initRangePicker();
         },
         tokenshow: function () {
@@ -54,10 +56,10 @@ var vm = new Vue({
         initRangePicker: function () {
             $("#rangepicker").daterangepicker({
                 locale: {
-                    format: format
+                    format: vm.options.dateFormat
                 },
-                startDate: vm.info.callsChart.startDate,
-                endDate: vm.info.callsChart.endDate,
+                startDate: vm.options.callsChart.startDate,
+                endDate: vm.options.callsChart.endDate,
                 ranges: {
                     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                     'Last 31 Days': [moment().subtract(30, 'days'), moment()],
@@ -71,76 +73,99 @@ var vm = new Vue({
                 maxDate: moment(),
                 autoApply: true
             }).on('apply.daterangepicker', function (ev, picker) {
-                vm.info.days = Math.abs(picker.startDate.diff(moment(), 'days') - 1);
-                vm.info.callsChart.startDate = picker.startDate;
-                vm.info.callsChart.endDate = picker.endDate;
-                vm.requestData(vm.info.token, vm.info.days);
+                vm.options.callsChart.days = Math.abs(picker.startDate.diff(moment(), 'days') - 1);
+                vm.options.callsChart.startDate = picker.startDate;
+                vm.options.callsChart.endDate = picker.endDate;
+                vm._requestData(vm.info.token, vm.options.callsChart.days);
             });
+        },
+        _checkRefreshNeeded: function (days) {
+
+            if (this.info.range === null) {
+                console.log("Cached data not present.");
+                return true;
+            }
+
+            var refreshNeeded = false;
+
+            var nowFormatted = moment().format('YYYY-MM-DD');
+            var toFormatted = moment(this.info.range.to).format('YYYY-MM-DD');
+            var fromDiff = Math.abs(moment(this.info.range.from).diff(moment(nowFormatted), 'days'));
+
+            //console.log("Comparing NOW (" + nowFormatted + ") VS TO (" + toFormatted + ")");
+            if (moment(nowFormatted).isAfter(moment(toFormatted))) {
+                //console.log("Now Formatted (" + nowFormatted + ") is after To Formatted (" + toFormatted + ")");
+                refreshNeeded = true;
+            }
+
+            //console.log("Comparing oldest saved date's diff (" + fromDiff + ") to today is less than number of requested days (" + days + ")");
+            if (fromDiff < days) {
+                //console.log("Least recent saved date's diff (" + fromDiff + ") to today is less than number of requested days (" + days + ")");
+                refreshNeeded = true;
+            }
+
+            return refreshNeeded;
+        },
+        _filterCallRange: function (calls) {
+            var keys = Object.keys(calls);
+            var dlen = keys.length;
+
+            var startCheck = moment(this.options.callsChart.startDate.format()).subtract(1, 'days');
+            var endCheck = this.options.callsChart.endDate;
+
+            for (var i = 0; i < dlen; i++) {
+                if (!moment(keys[i]).isBetween(startCheck, endCheck)) {
+                    delete calls[keys[i]];
+                }
+            }
+            return calls;
         },
         // Note that on the API end, if less than 31 days are given, it defaults back to 31
         // This is because shorter timespans don't get returned faster, and we do all the charting
         // and calculating on the client side anyway, so there's no point in doing short-range fetches
-        requestData: function (token, days) {
+        _requestData: function (token, days) {
             if (days === undefined || days === null) {
                 days = 31;
             }
             this.showOverlay(true);
-            this.$http.get('/api.php', {
-                "token": token,
-                "days": days
-            }, function (data, status, request) {
 
-                if (data.status == "OK") {
-
-                    var keys = Object.keys(data['data']['calls']);
-                    var dlen = keys.length;
-
-                    for (var i = 0; i < dlen; i++) {
-                        var startCheck = moment(this.info.callsChart.startDate.format()).subtract(1, 'days');
-                        var endCheck = this.info.callsChart.endDate;
-                        if (!moment(keys[i]).isBetween(startCheck, endCheck)) {
-                            delete data['data']['calls'][keys[i]];
-                        }
+            if (this._checkRefreshNeeded(days)) {
+                this.$http.get(this.options.apiUrl, {
+                    "token": token,
+                    "days": days
+                }, function (data, status, request) {
+                    if (data.status == "OK") {
+                        data['data']['token'] = token;
+                        this._saveData(data['data']);
+                        this.chartData(this._filterCallRange(jQuery.extend(true, {}, data['data']['calls'])));
+                        this.renderInvoices(data['data']['invoices']);
+                    } else {
+                        localStorage.clear();
+                        this.info = defaultInfo;
+                        swal("Oh noes!", "Looks like something went wrong: " + data.message + " (code: " + data.code + ")", "error");
                     }
-
-                    data['data']['token'] = token;
-                    this.saveData(data['data']);
-                    this.chartData(data['data']['calls']);
-
-                } else {
-                    this.resetInfo();
-                    swal("Oh noes!", "Looks like something went wrong: " + data.message + " (code: " + data.code + ")", "error");
-                }
-
-                this.showOverlay(false);
-            });
-        },
-        saveData: function (data) {
-            if (data !== undefined) {
-                this.saveData();
-                for (var property in data) {
-                    if (data.hasOwnProperty(property)) {
-                        localStorage.setItem(property, data[property]);
-                        this.info[property] = data[property];
-                    }
-                }
+                    this.showOverlay(false);
+                });
             } else {
-                localStorage.clear();
+                // Refresh not needed
+                this.chartData(this._filterCallRange(jQuery.extend(true, {}, this.info.calls)));
+                this.renderInvoices(this.info.invoices);
+                this.showOverlay(false);
             }
         },
-        loadData: function () {
-            return {
-                "name": localStorage.getItem("name"),
-                "plan": localStorage.getItem("plan"),
-                "token": localStorage.getItem("token"),
-                "days": localStorage.getItem("days") || 31
-            }
+        _saveData: function (data) {
+            localStorage.setItem('cached', JSON.stringify(data));
+            this._loadData();
         },
-        resetInfo: function () {
-            this.info = info;
+        _loadData: function () {
+            var cached = localStorage.getItem('cached');
+            if (cached !== null) {
+                cached = JSON.parse(cached);
+            }
+            this.info = jQuery.extend(true, this.info, cached);
         },
         showOverlay: function (bool) {
-            this.ui.overlay = bool;
+            this.options.overlay = bool;
         },
         chartData: function (data) {
 
@@ -170,6 +195,9 @@ var vm = new Vue({
                     }
                 }
             });
+        },
+        renderInvoices: function(data) {
+
         }
     },
     http: {
--- public/api.php
+++ public/api.php
@@ -11,11 +11,32 @@ try {
     $diffbot = new Swader\Diffbot\Diffbot($_GET['token']);
     $days = (isset($_GET['days']) && (int)$_GET['days'] > 31) ? (int)$_GET['days'] : 31;
     $info = $diffbot->getAccountInfo($days);
+
+    $cpd = $info->getCallsPerDay();
+    $keys = array_keys($cpd);
+    $to = key($cpd);
+    $from = array_pop($keys);
+
+    $invoices = $info->getInvoices();
+    // Fake overage
+    $invoices = array_merge($invoices, $invoices, $invoices, $invoices);
+    foreach ($invoices as &$invoice) {
+        if (rand(1,20) > 15) {
+            $invoice['overageAmount'] = rand(1,1000);
+            $invoice['status'] = 'foo';
+        }
+    }
+
     $payload = [
         "data" => [
             'name' => $info->getName(),
             'plan' => $info->getPlan(),
-            'calls' => $info->getCallsPerDay()
+            'calls' => $cpd,
+            'invoices' => $invoices,
+            'range' => [
+                'from' => $from,
+                'to' => $to
+            ]
         ],
         "status" => "OK"
     ];
