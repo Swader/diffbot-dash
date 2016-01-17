a82542a4927e90f3c9d4ad1fed82eff952199f7e
Bruno Skvorc <bruno@skvorc.me>
2016-01-17 19:12:51 +0100

    Reorganized files a bit, removed old files

--- assets/js/new/components/something.js
+++
@@ -1,9 +0,0 @@
-var Something = Vue.extend({
-    template: `
-        <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-        Hello there!
-        </section></div>
-    `
-});
